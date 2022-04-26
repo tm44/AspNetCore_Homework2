ff0995c08f7ce5e456402b1579d2db91f92c4de9
Mike Tate <shagee@uw.edu>
2022-04-26 16:50:23 -0700

    Added homework2

---
+++ Homework2/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Homework2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Homework2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string sortOrder)
+        {
+            var contacts = new[]
+            {
+        new Contact{Id = 1, Name="dave", City="Seattle", State="WA", Phone="123"},
+        new Contact{Id = 2, Name="mike", City="Spokane", State="WA", Phone="234"},
+        new Contact{Id = 3, Name="lisa", City="San Jose", State="CA", Phone="345"},
+        new Contact{Id = 4, Name="cathy", City="Dallas", State="TX", Phone="456"},
+    };
+
+            if (sortOrder != null)
+            {
+                switch (sortOrder.ToLower())
+                {
+                    case "id":
+                        {
+                            contacts = contacts.OrderBy(c => c.Id).ToArray<Contact>();
+                            break;
+                        }
+                    case "name":
+                        {
+                            contacts = contacts.OrderBy(c => c.Name).ToArray<Contact>();
+                            break;
+                        }
+                    case "city":
+                        {
+                            contacts = contacts.OrderBy(c => c.City).ToArray<Contact>();
+                            break;
+                        }
+                    case "state":
+                        {
+                            contacts = contacts.OrderBy(c => c.State).ToArray<Contact>();
+                            break;
+                        }
+                    case "phone":
+                        {
+                            contacts = contacts.OrderBy(c => c.Phone).ToArray<Contact>();
+                            break;
+                        }
+                }
+            }
+
+            return View(contacts);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Homework2/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace Homework2.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+    }
+}
